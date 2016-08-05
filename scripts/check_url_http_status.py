e1d91c4e0237db63fdbf5948dd13b47bcdb6ed30
Janet Riley <jriley@janetriley.net>
2016-08-05 17:06:52 -0400

    don't copy DONE links to redis, let them go

--- scripts/check_url_http_status.py
+++ scripts/check_url_http_status.py
@@ -1,8 +1,8 @@
-import datetime
 import os
+import datetime
 
+import neat_as_a_pin.src.pinboard
 from neat_as_a_pin.conf import config
-from neat_as_a_pin.src import common
 from neat_as_a_pin.src import url_checker
 from neat_as_a_pin.src import url_store
 
@@ -13,15 +13,16 @@ Save results to a redis queue based on HTTP status and next action to take.
 
 if __name__ == "__main__":
     pinboard_bookmarks_file = os.environ.get('FILE') or (config.DATA_DIR + '/pinboard_export.json')
-    bookmarks = common.get_bookmarks_from_file(pinboard_bookmarks_file)
+
+    bookmarks = neat_as_a_pin.src.pinboard.read_pinboard_exports(pinboard_bookmarks_file)
 
     counter = 0
 
     for bookmark in bookmarks:
         result = url_checker.check_status_and_make_bookmark(bookmark)
         queue = url_store.status_to_queue(result.status)
-        url_store.add(queue, result, autosave=False)
-
+        if queue is not url_store.DONE:
+            url_store.add(queue, result, autosave=False)
         counter += 1
 
         if counter % 100 == 0:
